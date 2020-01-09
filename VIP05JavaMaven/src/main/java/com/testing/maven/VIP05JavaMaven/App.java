8e6456c3c95a57418085ad69fbf6d85f0b65b763
lisa <185799@qq.com>
2020-01-09 22:16:15 +0800

    Signed-off-by: lisa <185799@qq.com>

---
+++ VIP05JavaMaven/src/main/java/com/testing/maven/VIP05JavaMaven/App.java
@@ -0,0 +1,13 @@
+package com.testing.maven.VIP05JavaMaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
